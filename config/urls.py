7b923457f36a8114471d7b155c3f0e6e14fc26be
Miloslav Staněk <milostanek@gmail.com>
2026-04-20 19:54:54 +0200

    fix(URLs): Fix URLs order

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("apps.accounts.urls")),
     path("", include("apps.catalog.urls")),
     path("", include("apps.reviews.urls")),
-    path("accounts/", include("apps.accounts.urls")),
-    path("admin/", admin.site.urls),
 ]
